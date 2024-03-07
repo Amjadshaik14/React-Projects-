bd126bbefcd2690ac6caec907918348d0bfe833a
Amjad Shaik <amjadshaik477@gmail.com>
2024-03-07 20:50:52 +0530

    Adding the statistics functionality using derived state

--- Trip to-do list/src/App.jsx
+++ Trip to-do list/src/App.jsx
@@ -25,7 +25,7 @@ function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -108,10 +108,24 @@ function Item({ item, onDeleteItem, onToggleItem }) {
     </li>
   );
 }
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your list</em>
+      </p>
+    );
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const packedPercentage = Math.round((numPacked / numItems) * 100);
   return (
     <footer className="stats">
-      <em>You have X items on your list, and you already packed X (x%)</em>
+      <em>
+        {packedPercentage === 100
+          ? "You got everything! Ready to go"
+          : `You have ${numItems} items on your list, and you already packed 
+        ${numPacked} (${packedPercentage}%)`}
+      </em>
     </footer>
   );
 }
