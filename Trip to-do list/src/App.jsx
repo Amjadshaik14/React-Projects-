481c1c90753efbbd46e8c4dfc81b0dcf2c58fe9d
Amjad Shaik <amjadshaik477@gmail.com>
2024-03-07 20:05:56 +0530

    bug in initialItems array

--- Trip to-do list/src/App.jsx
+++ Trip to-do list/src/App.jsx
@@ -35,7 +35,7 @@ export default App;
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "Charger", quantity: 1, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
 ];
 
 function Logo() {
